faef8228829688168e42b033d9c8f1811d5f63a7
Zad0ya <zadoyamaxim@gmail.com>
2023-02-27 01:56:14 +0100

    first commit

---
+++ PraktOOP1/1program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    delegate double Calculation(double a, double b);
+
+    static void Main(string[] args)
+    {
+        Calculation ADD = (a, b) => a + b;
+        Calculation SUB = (a, b) => a - b;
+        Calculation MUL = (a, b) => a * b;
+        Calculation DIV = (a, b) => {
+                if (a == 0 || b == 0)
+                {
+                    throw new DivideByZeroException("На нуль ділити не можна.");
+                }
+                return a / b;
+            };
+            Console.WriteLine("Введіть перше число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть друге число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Виберіть дію +, -, *, / :");
+            string nums = Console.ReadLine();
+            double numres;
+            switch (nums)
+            {
+                case "+":
+                    numres = ADD(a, b);
+                    break;
+                case "-":
+                    numres = SUB(a, b);
+                    break;
+                case "*":
+                    numres = MUL(a, b);
+                    break;
+                case "/":
+                    numres = DIV(a, b);
+                    break;
+                default:
+                    throw new ArgumentException("Невідома операція .");
+            }
+            Console.WriteLine("Відповідь: " + numres);
+        
+
+    Console.ReadLine();
+    }
+}
---
+++ PraktOOP1/secprogram.cs
@@ -0,0 +1,17 @@
+﻿class secprogram
+{
+    static void Main(string[] args)
+    {
+        Func<int, int, int, double> arithmeticMean = delegate (int a, int b, int c)
+        {
+            return (a + b + c) / 3.0;
+        };
+        Console.WriteLine("Введіть тpи числа:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int num3 = Convert.ToInt32(Console.ReadLine());
+        double result = arithmeticMean(num1, num2, num3);
+        Console.WriteLine("Середнє арифметичне {0}, {1} і {2} дорівнює {3}.", num1, num2, num3, result);
+        Console.ReadKey();
+    }
+}
